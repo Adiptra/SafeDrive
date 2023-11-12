5183880a6f64a1b43cb1b78593efa76464bf06a2
Adiptra <adiirawan2705@gmail.com>
2023-11-12 23:46:41 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        PlayRg: ["PlayRg", "sans-serif"],
+        PlayBd: ["PlayBd", "sans-serif"],
+        MontserratRg: ["MontserratRg", "sans-serif"],
+        MontserratSb: ["MontserratSb", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
